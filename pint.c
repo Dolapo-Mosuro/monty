c1ce36a3a1e149c066c6bed988333c176ccaf934
Dolapo-mosuro <mosurodolapo@gmail.com>
2024-01-18 02:22:21 -0800

    betty check pintcode

--- pint.c
+++ pint.c
@@ -1,24 +1,25 @@
 #include "monty.h"
 /**
- *pint- function to push an element to a stack
- *@num: element that gets pushed
+ *pint - function to prints the value at the top of the stack
+ *@stack: top of the stack
+ *@line_number: on error of line
  *Return: nothing
  */
 void pint(stack_t **stack, unsigned int line_number)
 {
 	stack_t *current;
+
 	current = *stack;
 	if (current == NULL)
 	{
-		fprintf(stderr,"%d\n",line_number);
+		fprintf(stderr, "%d\n", line_number);
 		exit(EXIT_FAILURE);
 
-	}else
+	} else
 	{
-		if(current!=NULL)
+		if (current != NULL)
 		{
-			printf("%d\n",current->n);
-	     
+			printf("%d\n", current->n);
 		}
 	}
-}	
+}
