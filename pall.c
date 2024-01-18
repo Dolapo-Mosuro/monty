cc23daea79a257a81ac8f6a139b1442943fb1109
Dolapo-mosuro <mosurodolapo@gmail.com>
2024-01-18 03:02:15 -0800

    betty checks for pall.c

--- pall.c
+++ pall.c
@@ -1,24 +1,27 @@
 #include "monty.h"
 /**
- *push- function to push an element to a stack
- *@num: element that gets pushed
+ *pall - function to push an element to a stack
+ *@stack: top of the stack
+ *@line_number: on error of line
  *Return: nothing
  */
 void pall(stack_t **stack, unsigned int line_number)
 {
 	stack_t *current;
+
 	current = *stack;
 	if (current == NULL)
 	{
-		fprintf(stderr,"%d\n",line_number);
+		fprintf(stderr, "%d\n", line_number);
 		exit(EXIT_FAILURE);
 
-	}else
+	}
+	else
 	{
-		while(current)
+		while (current)
 		{
-			printf("%d\n",current->n);
+			printf("%d\n", current->n);
 			current = current->next;
 		}
 	}
-}	
+}
--- push.c
+++ push.c
@@ -1,24 +1,26 @@
 #include "monty.h"
 /**
  *push- function to push an element to a stack
- *@num: element that gets pushed
- *Return: nothing
+ *@stack: beginning at the top of the stack
+ *@line_number: print the error message
+ *@_isdigit
+*Return: nothing
  */
 void push(stack_t **stack, unsigned int line_number)
-{	
+{
 	stack_t *current;
 	stack_t *new_node;
 	int result;
 
-	if(_isdigit(argument) == 0)
+	if (_isdigit(argument) == 0)
 	{
-		fprintf(stderr,"%d: usage: push integer",line_number);
+		fprintf(stderr, "%d: usage: push integer", line_number);
 	}
 
 	result = atoi(argument);
 	current = *stack;
 	new_node = malloc(sizeof(stack_t));
-	if(new_node == NULL)
+	if (new_node == NULL)
 	{
 		exit(EXIT_FAILURE);
 	}
@@ -29,7 +31,7 @@ void push(stack_t **stack, unsigned int line_number)
 		new_node->n = result;
 		*stack = new_node;
 	}
-	else 
+	else
 	{
 		new_node->n = result;
 		new_node->prev = NULL;
@@ -39,11 +41,17 @@ void push(stack_t **stack, unsigned int line_number)
 	}
 }
 
+/**
+ *_isdigit - function to check if the argument is a number
+ *@str: the initial string
+ *Return: nothing
+ */
+
 int _isdigit(char *str)
 {
 	char *ptr = str;
-	
-	while(*ptr)
+
+	while (*ptr)
 	{
 		if (*ptr <  '0' || *ptr > '9')
 			return (0);
