50f757dbdaf440d64ee334c19d0175cd89004fa9
Dolapo-mosuro <mosurodolapo@gmail.com>
2024-01-19 04:51:36 -0800

    nop opcodes correction

--- nop.c
+++ nop.c
@@ -1,8 +1,12 @@
 #include "monty.h"
 /**
- *nop - function doesn't do anything
+ *_nop - function doesn't do anything
  *@stack: top of the stack
  *@line_number: on error of line
  *Return: nothing
  */
-void nop()
+void _nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack_t;
+	(void)line_number;
+}
