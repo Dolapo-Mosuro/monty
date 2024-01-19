5700d45ea786c7a4d23b7091c3e717eada4777f2
Dolapo-mosuro <mosurodolapo@gmail.com>
2024-01-19 04:33:06 -0800

    nop opcode

---
+++ nop.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+ *nop - function doesn't do anything
+ *@stack: top of the stack
+ *@line_number: on error of line
+ *Return: nothing
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+
+}
