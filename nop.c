67d617028c316021a155f7c528e3f7a9d86ff6b2
Dolapo-mosuro <mosurodolapo@gmail.com>
2024-01-20 08:59:20 -0800

    nop corrections

--- nop.c
+++ nop.c
@@ -5,7 +5,7 @@
  *@line_number: on error of line
  *Return: nothing
  */
-void nop(stack_t **stack, unsigned int line_number)
+void nop(stack_t **stack, unsigned int line_number);
 {
 	(void)stack;
 	(void)line_number;
